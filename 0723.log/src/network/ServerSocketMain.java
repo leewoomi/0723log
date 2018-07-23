96bc96d7b60bc3a9af2bf3895c312eb7aa0d3c46
leewoomi <woomi1114@gmail.com>
2018-07-23 17:26:18 +0900

    network

--- 0723.log/src/network/ServerSocketMain.java
+++ 0723.log/src/network/ServerSocketMain.java
@@ -6,7 +6,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
-
+//ServerSocketMain 먼저 실행
 public class ServerSocketMain {
 
 	public static void main(String[] args) {
